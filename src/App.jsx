bbd353a78462579d462793f393cc74a17b784f6e
Samarth <subedi42samarth@gmail.com>
2026-07-09 08:15:18 +0545

    styling: flex

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import { Routes, Route, Link, Outlet } from "react-router";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import "./App.css"
 
 // from   
 function Layout() {
   return (
-    <>
+    <div className="header">
       <nav>
         <Link to="/"> Home </Link>
-        <br />     
+
         <Link to="/about"> About </Link>
         <Link to="/contact"> Contact </Link>
       </nav>
       <Outlet />
-    </>
+    </div>
   );
 }
 // to
